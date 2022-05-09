5bec3e60fda5bfa3aa360c7f5d744605d61bed23
Rostislav Salomatin <rsalomatin@yandex.ru>
2022-05-09 13:23:07 +0300

    homework2.2

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.netology.nmedia.classes.*
 import ru.netology.nmedia.databinding.PostBinding
+import ru.netology.nmedia.viewModel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,37 +12,29 @@ class MainActivity : AppCompatActivity() {
         val binding = PostBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val post = Post(
-            id = 1,
-            author = "Вася Пупкин",
-            content = "Всем привет! Это мой первый пост! УРА!!!",
-            published = "28 апреля в 13:45"
-        )
+        val viewModel = PostViewModel()
 
-        with(binding) {
-            author.text = post.author
-            published.text = post.published
-            content.text = post.content
-            if (post.likedByMe) {
-                likes.setImageResource(R.drawable.ic_liked_24dp)
-            }
-            likesCount.text = convertCount(post.likedCount)
-            likes.setOnClickListener {
-                post.likedByMe = !post.likedByMe
+        viewModel.data.observe(this) { post ->
+            with(binding) {
+                author.text = post.author
+                published.text = post.published
+                content.text = post.content
                 if (post.likedByMe) {
                     likes.setImageResource(R.drawable.ic_liked_24dp)
-                    post.likedCount++
                 } else {
                     likes.setImageResource(R.drawable.ic_likes_24dp)
-                    post.likedCount--
                 }
                 likesCount.text = convertCount(post.likedCount)
-            }
-
-            shares.setOnClickListener {
-                post.sharedCount++
                 sharesCount.text = convertCount(post.sharedCount)
             }
+        }
+
+        binding.likes.setOnClickListener {
+            viewModel.like()
+        }
+
+        binding.shares.setOnClickListener {
+            viewModel.share()
         }
     }
 }
