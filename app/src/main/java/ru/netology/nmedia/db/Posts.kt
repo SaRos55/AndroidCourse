ddd28bdcca486ae18ab8ad5cb3c24483ba999fc0
Rostislav Salomatin <rsalomatin@yandex.ru>
2022-07-15 16:17:27 +0300

    homework 4.1

---
+++ app/src/main/java/ru/netology/nmedia/db/Posts.kt
@@ -0,0 +1,14 @@
+package ru.netology.nmedia.db
+
+import android.database.Cursor
+import ru.netology.nmedia.classes.Post
+
+fun Cursor.toPost() = Post(
+    id = getLong(getColumnIndexOrThrow(PostsTable.Column.ID.columnName)),
+    author = getString(getColumnIndexOrThrow(PostsTable.Column.AUTHOR.columnName)),
+    content = getString(getColumnIndexOrThrow(PostsTable.Column.CONTENT.columnName)),
+    published = getString(getColumnIndexOrThrow(PostsTable.Column.PUBLISHED.columnName)),
+    likedCount = getInt(getColumnIndexOrThrow(PostsTable.Column.LIKES.columnName)),
+    likedByMe = getInt(getColumnIndexOrThrow(PostsTable.Column.LIKED_BY_ME.columnName)) != 0,
+    sharedCount = getInt(getColumnIndexOrThrow(PostsTable.Column.SHARES.columnName))
+    )
